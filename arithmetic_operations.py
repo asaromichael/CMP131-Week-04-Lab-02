0c0c75ab41436b729f46d6b3c90cde62bc2a5844
asaromichael <asaro.michael@student.ccm.edu>
2026-09-24 12:09:02 -0400

    Started arithmetic_operations

---
+++ arithmetic_operations.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the first number."))
+num2 = float(input("Enter the second number, this must not be a zero."))
+
+addition = num1 + num2
+print("The numbers added is ", addition)
+
+subtraction = num1 - num2
+print("The second number subtracted from the first is ", subtraction)
