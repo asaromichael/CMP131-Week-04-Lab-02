cabfc16a752a0844065b0af00f0224c62d7fa80f
asaromichael <asaro.michael@student.ccm.edu>
2026-09-24 22:28:06 -0400

    Finished Lab 2

--- AI-Use-Report.md
+++ AI-Use-Report.md
@@ -2,17 +2,17 @@
 
 ## Student Information
 
-- Student name:
-- Week:
-- Lab:
-- Date:
+- Student name: Michael Asaro
+- Week: 4
+- Lab: 2
+- Date: 9/24/2026
 
 ## AI Use
 
 Did you use an AI tool for this lab?
 
 - [ ] Yes
-- [ ] No
+- [No] No
 
 If yes, complete the sections below. If no, write “No AI tool was used” under Summary.
 
@@ -50,4 +50,4 @@ Response:
 
 Provide any additional information about your use of AI for this lab.
 
-Response:
+Response: I didnt use ai
--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -6,3 +6,15 @@ print("The numbers added is ", addition)
 
 subtraction = num1 - num2
 print("The second number subtracted from the first is ", subtraction)
+
+multiplication = num1 * num2
+print("The numbers multiplied is ", multiplication)
+
+division = num1 / num2
+print("The first number divided by the second is ", division)
+
+power = num1 ** num2
+print("The first number raised to the second number is ", power)
+
+average = (num1 + num2) / 2
+print("The average of the two numbers is ", average)
